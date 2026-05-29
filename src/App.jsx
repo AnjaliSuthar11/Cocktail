a39fb7cf7c51536c5f61487ccf0848590885dcd5
Your Name <anjalisuthar114@gmail.com>
2026-05-29 15:12:31 +0530

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {ScrollTrigger,SplitText} from 'gsap/all'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
-import About from './components/about';
+import About from './components/About'
 import Art from './components/Art';
 import Menu from './components/Menu';
 import Contact from './components/Contact';
